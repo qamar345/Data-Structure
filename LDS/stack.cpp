26bf2b49eaf6088ee3762fd52d04ad44ccc62d99
Qamar Naveed <80244665+qamar345@users.noreply.github.com>
2023-10-26 16:52:37 +0000

    Stack Implementation in C++

--- LDS/stack.cpp
+++ LDS/stack.cpp
@@ -2,108 +2,92 @@
 using namespace std;
 
 #define MAX 5
-// int size = 0;
 
-class stack
-{
+
+class stack{
+
     int items[MAX];
     int top = -1;
-    // int size;
 
-public:
-    // stack()
-    // {
-    //     int top = -1;
-    // }
 
-    int isFull()
-    {
-        if (top == MAX - 1)
-        {
+    public:
+
+    int isEmpty(){
+        if(top == -1){
             return 1;
-        }
-        else
-        {
+        }else{
             return 0;
         }
     }
 
-    int isEmpty()
-    {
-        if (top == -1)
-        {
+    int isFull(){
+        if(top == MAX - 1){
             return 1;
-        }
-        else
-        {
+        }else{
             return 0;
         }
     }
 
-    void push(int newItem)
-    {
-        if (isFull())
-        {
+    void push(int newItem){
+        if(isFull()){
             cout << "Stack is Full" << endl;
-        }
-        else
-        {
-            cout <<"Bfore: " << top << endl;
+        }else{
+            cout << "Before Push Element in Stack The Value of Top is " << top << endl << endl;
             top++;
             items[top] = newItem;
-            cout <<"After: " << top << endl;
+            cout << "After Push Element in Stack The Value of Top is " << top << endl << endl;
         }
-        // size++;
     }
 
-    void pop()
-    {
-        if (isEmpty())
-        {
-            cout << "Stack is Empty" << endl;
-        }
-        else
-        {
+    void pop(){
+        if(isEmpty()){
+            cout << "Stack is Empty" << endl << endl;
+        }else{
             top--;
-            cout << "Element Popped from Stack" << endl;
+            items[top];
+            cout << "Pop Element from Stack" << endl << endl;
         }
-        // size--;
     }
 
-    void display()
-    {
-
-        for (int i = 0; i <= top; i++)
-        {
+    void display(){
+        for(int i = 0; i <= top; i++){
             cout << items[i] << " ";
         }
         cout << endl;
     }
 };
 
-int main()
-{
+int main(){
+
     stack s;
+
+    cout << "Before Push" << endl << endl;
+    s.display();
 
     s.push(10);
     s.push(20);
     s.push(30);
     s.push(40);
     s.push(50);
-    // s.push(50);
+    s.push(60);
+
+    cout << "After Push" << endl << endl;
 
     s.display();
 
+    cout << endl;
 
+    cout << "Pop Element from Stack with LIFO Principal" << endl << endl;
     s.pop();
     s.pop();
     s.pop();
     s.pop();
-    // s.pop();
-    // s.pop();
+    s.pop();
+    s.pop();
 
 
-
+    cout << "After Pop" << endl;
     s.display();
+
     return 0;
 }
